df6afb93ac69e6b2d42490154949f7f2f441a072
lrf <l_rongfang@163.com>
2016-07-22 16:41:25 +0800

    Toast出现

---
+++ app/src/main/java/com/aa/activitylifecycle/AntivitySecond.java
@@ -0,0 +1,9 @@
+package com.aa.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class AntivitySecond extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/aa/activitylifecycle/MainActivity.java
@@ -0,0 +1,59 @@
+package com.aa.activitylifecycle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        Button button = (Button) findViewById(R.id.activity_main_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent (MainActivity.this,AntivitySecond.class);
+//                startActivity(intent);
+                Toast.makeText(MainActivity.this, "xxxxxx", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
+
